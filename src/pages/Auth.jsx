50dea8f40a050c75d67564e99289781b096a40e0
Omkar More <omkar.pradipmore005@gmail.com>
2024-07-19 13:12:45 +0530

    sec commit

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -16,7 +16,7 @@ function Auth() {
       // api request to login or register
 
       const { data } = await axios.post(
-        `http://localhost:3001/api/users${isRegister ? "" : "/login"}`,
+        `https://blogging-backend-1-1eyg.onrender.com/api/users${isRegister ? "" : "/login"}`,
         { user: values }
       );
 
